9a26edf8ace37ca89ecf39d75b0b9151bb461d22
Jose2709 <luis_jose1978@hotmail.com>
2026-04-22 15:29:04 -0500

    Colas - genericos - ejercicio1

--- src/main/java/uni/edu/pe/Queues/Cola1/IQueue.java
+++ src/main/java/uni/edu/pe/Queues/Cola1/IQueue.java
@@ -1,4 +1,11 @@
 package uni.edu.pe.Queues.Cola1;
 
-public interface IQueue {
+public interface IQueue<T> {
+
+    public void enqueue(T name);
+    public T dequeue();
+    public boolean isEmpty();
+    public T getHead();
+    public T getTail();
+    public void recorre();
 }
